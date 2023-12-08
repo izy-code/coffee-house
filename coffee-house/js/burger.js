4efd53068243f4b6ba2807ee23767e40ff435e6d
Izy <126877709+izy-code@users.noreply.github.com>
2023-12-08 13:08:25 +0300

    feat: add transition effect delay for burger links

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -1,4 +1,4 @@
-const WIDTH_TABLET= 768;
+const WIDTH_TABLET = 768;
 
 const header = document.querySelector('.header');
 const nav = header.querySelector('.header__nav');
@@ -18,6 +18,30 @@ const closeBurgerMenu = () => {
   burgerButtonTextNode.textContent = 'Open navigation';
 };
 
+const onBurgerButtonClick = () => {
+  if (header.classList.contains('header--js-burger-opened')) {
+    closeBurgerMenu();
+  } else {
+    openBurgerMenu();
+  }
+};
+
+const onNavClick = (evt) => {
+  const linkNode = evt.target.closest('.nav__link');
+
+  if (linkNode && header.classList.contains('header--js-burger-opened')) {
+    if (linkNode.matches('a[href]')) {
+      evt.preventDefault();
+
+      nav.addEventListener('transitionend', () => {
+        window.location.href = linkNode.href;
+      }, { once: true });
+    }
+
+    closeBurgerMenu();
+  }
+};
+
 const onWindowResize = () => {
   if (window.innerWidth > WIDTH_TABLET) {
     nav.classList.remove('header__nav--js-transition-added');
@@ -27,22 +51,8 @@ const onWindowResize = () => {
 };
 
 const addBurgerHandlers = () => {
-  burgerButton.addEventListener('click', () => {
-    if (header.classList.contains('header--js-burger-opened')) {
-      closeBurgerMenu();
-    } else {
-      openBurgerMenu();
-    }
-  });
-
-  nav.addEventListener('click', (evt) => {
-    const linkNode = evt.target.closest('.nav__link');
-
-    if ((linkNode) && (header.classList.contains('header--js-burger-opened'))) {
-      closeBurgerMenu();
-    }
-  });
-
+  burgerButton.addEventListener('click', onBurgerButtonClick);
+  nav.addEventListener('click', onNavClick);
   window.addEventListener('resize', onWindowResize);
 };
 
