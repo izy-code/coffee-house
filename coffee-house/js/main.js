1e1426781f58e26b97ecb13c1c27c52125983422
Izy <126877709+izy-code@users.noreply.github.com>
2023-12-10 23:37:27 +0300

    feat: add categories of products on the menu page

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -2,6 +2,14 @@ import { addBurgerHandlers } from './burger.js';
 
 addBurgerHandlers();
 
+if (document.querySelector('.menu') !== null) {
+  const dataModule = await import('./data.js');
+  const menuModule = await import('./menu.js');
+  const products = await dataModule.getDataWithIds();
+
+  menuModule.initMenu(products);
+}
+
 if (document.querySelector('.carousel') !== null) {
   const carouselModule = await import('./carousel.js');
   carouselModule.initCarousel();
